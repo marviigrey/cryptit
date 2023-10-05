01237cdd286bdb8178c69c1a64c1a2f9a6394064
marviigrey <marvellousfrederick@gmail.com>
2023-10-05 15:52:11 +0000

    crypit minor version change

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/marviigrey/cryptit
+
+go 1.20
+
+require github.com/marviigrey/learning-golang/projects/cryptit v0.0.0-20231005143531-4a2cc9e621dd
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/marviigrey/learning-golang/projects/cryptit/decrypt"
+	"github.com/marviigrey/learning-golang/projects/cryptit/encrypt"
+)
+
+func main() {
+	encryptedStr := encrypt.Nimbus("Kodekloud")
+	fmt.Println(encryptedStr)
+        fmt.Println(decrypt.Nimbus(encryptedStr))
+}
